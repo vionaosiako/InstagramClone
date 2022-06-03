53844bba229a02e25c11801317bd134bce9b9d97
vionaosiako <viona.osiako@student.morigaschool.com>
2022-06-03 23:27:43 +0300

    Add logout functionality

--- InstaApp/urls.py
+++ InstaApp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('registerPage', views.registerPage, name='registerPage'),
     path('loginPage', views.loginPage, name='loginPage'),
+    path('logout', views.logoutUser, name='logout'),
     
 ]
