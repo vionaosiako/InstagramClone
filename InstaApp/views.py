3e0a822e2f91378283cc1bd629d4e6d6ef944d5b
vionaosiako <viona.osiako@student.morigaschool.com>
2022-06-03 23:16:43 +0300

    Add login functionality

--- InstaApp/views.py
+++ InstaApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 # from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login,logout
 from .forms import  CreateUserForm
+# from django.contrib import messages
 # Create your views here.
 def registerPage(request):
     form =  CreateUserForm()
@@ -13,6 +15,16 @@ def registerPage(request):
             return redirect('loginPage')
     return render(request, 'auth/register.html', contex)
 def loginPage(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
     contex = {}
     return render(request, 'auth/login.html', contex)
 def index(request):
